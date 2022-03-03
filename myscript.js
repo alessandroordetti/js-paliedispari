f1446ad4e2f1b31aaabaedbdfc7b10c74992fdd6
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-03 20:10:08 +0100

    Update

--- myscript.js
+++ myscript.js
@@ -20,10 +20,12 @@ function isPalindromo (word) {
     /* Word.lenght da a lettersCount il numero di volte che dovrà ripetere il ciclo for */
     let lettersCount = word.length;
 
-    for (let i = 0; i < lettersCount; i++) {
+    for (let i = 0; i < word.lenght; i++) {
+
+        /* Se la lettera [0] è diversa dalla lettera [numero della lunghezza della parola - 1 - 0] allora la parola non è palindroma */
         if (word[i] !== word[lettersCount - 1 - i]) {
             console.log(word + " non è un nome palindromo");
-            return 'non è nome palindromo';
+            return 'non è un nome palindromo';
         } else {
             console.log(word + "è un nome palindromo");
         }
@@ -33,6 +35,15 @@ function isPalindromo (word) {
 
 let newWord = isPalindromo(word);
 
-document.getElementById('paragraph').innerHTML += ` : La parola ${newWord}`
+document.getElementById('paragraph').innerHTML += ` : La parola ${newWord}`;
+
+
+const oddOrEven = prompt('Scegli tra pari e dispari');
+
+let userNumber = parseInt(prompt('Scegli un numero tra uno e 5'));
+
+while (isNaN(userNumber) || userNumber > 5 || userNumber < 1) {
+    userNumber = parseInt(prompt('Scegli un numero tra uno e 5'));
+}
 
 
