d2f52e3f93e7facd53bcf93f4b9566c5e3cf1ad8
alessandroordetti <alessandro.ordetti@gmail.com>
2022-03-03 17:23:02 +0100

    Inizializzazione esercizio

---
+++ myscript.js
@@ -0,0 +1,11 @@
+let word = prompt('Scegli una parola');
+
+if (word != String) {
+    alert('Il computer ha sceclto per te la parola osso');
+    word = "osso";
+    console.log(word);
+}
+
+/* function isPalindroma () {
+    
+} */
